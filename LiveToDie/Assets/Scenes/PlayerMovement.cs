391f63cdcd82907f0658d003f327b5a82b4e5dff
exodus <alebar0102@gmail.com>
2020-09-13 14:48:52 +0200

    LeftRightMovement
    Main Character Movement Left And Right And Additional Animation Sprites For Different Vectors

---
+++ LiveToDie/Assets/Scenes/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Valami cumo h kiprobaljam mukszik e ez a megoldas");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
