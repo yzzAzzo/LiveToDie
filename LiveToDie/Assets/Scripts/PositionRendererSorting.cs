a42d7bbcd1a123459d810e43501a996f763cb83f
Baranyai Alexander <h845412@stud.u-szeged.hu>
2020-11-09 12:34:09 +0100

    Tile map(New Characters)

---
+++ LiveToDie/Assets/Scripts/PositionRendererSorting.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionRendererSorting : MonoBehaviour
+{
+    [SerializeField]
+    private int sortingOrderBase = 5000;
+
+    [SerializeField]
+    private int offset = 0;
+
+    private Renderer myRenderer;
+
+    private void Awake()
+    {
+        myRenderer = gameObject.GetComponent<Renderer>();
+    }
+
+    private void lateUpdate()
+    {
+        myRenderer.sortingOrder = (int)(sortingOrderBase - transform.position.y - offset);
+    }
+}
---
+++ LiveToDie/Assets/Scripts/TileAutomation.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEditor;
+
+public class TileAutomation : MonoBehaviour
+{
+    [Range(0,100)]
+    public int initialChance;
+    [Range(1,8)]
+    public int birthLimit;
+    [Range(1,8)]
+    public int deathLimit; 
+    [Range(1,10)]
+    public int numberOfReps;
+    private int count = 0;
+
+
+    private int[,] terrainMap;
+    public Vector3Int tileMapSize;
+    public Tilemap topMap;
+    public Tilemap botMap;
+    public Tile topTile;
+    public Tile bottomTilew;
+
+    int width;
+    int height;
+
+    public void doSimulation(int numberOfReps)
+    {
+        //clearMap(false);
+    }
+
+    public void clearMap()
+    {
+        topMap.ClearAllTiles();
+        botMap.ClearAllTiles();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
