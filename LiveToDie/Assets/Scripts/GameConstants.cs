55d4974762c8fca0e13f1351eca6f115f8ef707b
Baranyai Alexander <52284243+yzzAzzo@users.noreply.github.com>
2021-08-11 21:27:40 +0200

    LoadGame Menu Filled With Saved Maps Names

---
+++ LiveToDie/Assets/Scripts/GameConstants.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameConstants : MonoBehaviour
+{
+    public static readonly int NumberOfSaveableMaps = 4;
+    public static readonly string SavedGamesPath = "Assets/SavedMaps/";
+}
--- LiveToDie/Assets/Scripts/Menus/MainMenu.cs
+++ LiveToDie/Assets/Scripts/Menus/MainMenu.cs
@@ -1,23 +1,65 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    
-    public void NewGame()
-    {
-        GameManager.instance.NewGame();
-    }
 
-    public void LoadGame()
-    {
-        GameManager.instance.LoadGame();
-    }
-    public void Quit()
-    {
-        Application.Quit();    
-    }
+	public List<GameObject> Texts;
+	public List<Button> Buttons;
+
+
+	public void NewGame()
+	{
+		GameManager.instance.NewGame();
+	}
+
+	public void LoadGame()
+	{
+		GameManager.instance.LoadGame();
+	}
+	//TODO later on upgrade it(now only loads MAPS!)
+	public void ListLoadableGames()
+	{ 
+
+		List<Button> _loadableGameButtons = new List<Button>();
+		List<FileInfo> filteredInfos = new List<FileInfo>();
+
+		var info = new DirectoryInfo(GameConstants.SavedGamesPath);
+		var fileInfo = info.GetFiles();
+
+		//Load File Names
+		foreach(var file in fileInfo)
+        {
+            if (!file.Name.Contains(".meta"))
+            {
+				filteredInfos.Add(file);
+			}
+        }
+		
+		//Set Button Texts
+		for (int i = 0; i < GameConstants.NumberOfSaveableMaps; i++)
+		{
+			var check = Texts[i].GetComponent<TextMeshProUGUI>();
+
+            if (i < filteredInfos.Count)
+            {
+                check.text = filteredInfos[i].Name.Replace(".prefab", "");
+            }
+            else
+            {
+				check.text = "EMPTY";
+				Buttons[i].GetComponent<Button>().interactable = false;
+				Buttons[i].GetComponent<Image>().color = new Color32(169, 169, 169, 100);
+			}
+		}
+	}
+	public void Quit()
+	{
+		Application.Quit();    
+	}
 }
