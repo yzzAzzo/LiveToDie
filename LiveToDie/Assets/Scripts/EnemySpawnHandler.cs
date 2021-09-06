767873d44d465ab672dfbc1457fdbdd55515d555
Baranyai Alexander <52284243+yzzAzzo@users.noreply.github.com>
2021-09-06 19:13:02 +0200

    Changing Assets To Lava Land(Work In Progress)
    Changing Tile Generator tol lava land texture. Making new Rule Tile with extended neighbours + animated tiles for lava and rock smoke

--- LiveToDie/Assets/Scripts/EnemySpawnHandler.cs
+++ LiveToDie/Assets/Scripts/EnemySpawnHandler.cs
@@ -8,6 +8,7 @@ public class EnemySpawnHandler : MonoBehaviour
     public static EnemySpawnHandler instance;
     public Tilemap groundTileMap;
     public GameObject blueSlime;
+    public int spawnCap;
     // Start is called before the first frame update
     private void Awake()
     {
@@ -21,20 +22,21 @@ public class EnemySpawnHandler : MonoBehaviour
         Vector3Int coordinate = new Vector3Int(0, 0, 0);
         Vector3Int tilemapCenter = new Vector3Int(groundTileMap.size.x/2, groundTileMap.size.y/2, 0);
         //TODO delete unused params
-        for (int x = 0; x < groundTileMap.size.x &&  20 > count; x++)
+        for (int x = 0; x < groundTileMap.size.x && spawnCap > count; x++)
         {
-            for (int y = 0; y < groundTileMap.size.y && 20 > count; y++)
+            for (int y = 0; y < groundTileMap.size.y && spawnCap > count; y++)
             {
                 coordinate.x = x;
                 coordinate.y = y;
 
-                if (groundTileMap.HasTile(coordinate) && Vector3.Distance(coordinate, tilemapCenter) > 100)
+                if (groundTileMap.HasTile(coordinate) && Vector3.Distance(coordinate, tilemapCenter) > 50 && Vector3.Distance(coordinate, tilemapCenter) < 100)
                 {
                     if (Random.Range(0,100) > 80)
                     {
-                        Instantiate(blueSlime, coordinate, Quaternion.identity); 
+                        var worldPosition = groundTileMap.CellToWorld(coordinate);
+                        Instantiate(blueSlime, worldPosition, Quaternion.identity);
+                        count++;
                     }
-                    count++;
                 }
             }
         }
