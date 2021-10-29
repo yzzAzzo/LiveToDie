22b694117095b85635e05be7c24584e219e5d4a5
Baranyai Alexander <52284243+yzzAzzo@users.noreply.github.com>
2021-10-29 12:57:13 +0200

    Player animations walk attack

--- LiveToDie/Assets/Scripts/Player/Player.cs
+++ LiveToDie/Assets/Scripts/Player/Player.cs
@@ -4,42 +4,51 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
-{
+{ 
 
     public Animator animator;
     public float attackRange = 0.1f;
     public int damage = 20;
     public LayerMask enemyLayers;
     private Vector3 offset = new Vector3(0, 0.1f);
+    private bool isAttacking = false;
+    private float attackTime = 0f;
 
-
-    // Update is called once per frame
+    
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && attackTime <= 0 )
         {
-            Attack();
+             Attack();
+        }
+
+        if (isAttacking && (attackTime -= Time.deltaTime) <= 0)
+        {
+            isAttacking = false; 
+            animator.SetBool("IsAttacking", isAttacking);
+            //TODO mmake it work while moving.
         }
     }
 
     private void Attack()
     {
-        // Reference animator when we have it
-        //Animator.SetTrigger("Attack");
-
         Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(transform.position + offset, attackRange, enemyLayers);
+        isAttacking = true;
+        attackTime = 0.45f;
+
 
         foreach (Collider2D enemy in hitEnemies)
         {
-            Debug.Log("We hit" + enemy.name);
             enemy.GetComponent<Enemy>().TakeDamage(damage);
-
         }
+
+        animator.SetBool("IsAttacking", isAttacking);
         //Detect Enemy
     }
 
     void OnDrawGizmosSelected()
     {
-        Gizmos.DrawWireSphere(transform.position + offset, attackRange);
+        // Just To See The Attack Range
+        //Gizmos.DrawWireSphere(transform.position + offset, attackRange);
     }
 }
